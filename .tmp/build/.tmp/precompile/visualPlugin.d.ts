5b5897ddc27ffd2cf97ec6910e900d95dc559926
Yves Habersaat <yveshabersaat@hotmail.com>
2021-09-26 15:14:32 +0200

    Clean solution

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var reactVisualDemo004A41DA536548D0B1BCDED19492110E: IVisualPlugin;
-export default reactVisualDemo004A41DA536548D0B1BCDED19492110E;
--- .tmp/build/src/settings.d.ts
+++
@@ -1,9 +0,0 @@
-import { dataViewObjectsParser } from "powerbi-visuals-utils-dataviewutils";
-import DataViewObjectsParser = dataViewObjectsParser.DataViewObjectsParser;
-export declare class ReactVisualDemoSettings {
-    textColor: string;
-    textValue: string;
-}
-export declare class VisualSettings extends DataViewObjectsParser {
-    reactVisualDemo: ReactVisualDemoSettings;
-}
